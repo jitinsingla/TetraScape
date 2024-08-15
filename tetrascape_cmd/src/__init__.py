f461c3e37e1c45a36c6af0d13fba70d0149cee04
Jitin Singla <jitin2025@gmail.com>
2024-08-15 17:06:56 +0530

    Update __init__.py

--- tetrascape_cmd/src/__init__.py
+++ tetrascape_cmd/src/__init__.py
@@ -1,4 +1,4 @@
-from . import cmd
+from . import TetraScape
 from chimerax.atomic import ResiduesArg
 from chimerax.core.toolshed import BundleAPI
 from chimerax.core.commands import BoolArg, IntArg, CmdDesc, register, FloatArg
@@ -8,16 +8,16 @@ class _TetraAPI(BundleAPI):
 
 	@staticmethod
 	def register_command(bi, ci, logger):
-	    t_desc = CmdDesc(required = [], 
-	    				optional = [("residues", ResiduesArg), ("revSeq", BoolArg)], 
-	    				keyword = [],
-	    				synopsis = 'creates tetrahedral model')
-	    register('tetra', t_desc, cmd.tetrahedral_model, logger=logger)
+	    t_desc = CmdDesc(required = [],
+			     optional = [("residues", ResiduesArg), ("revSeq", BoolArg)],
+			     keyword = [],
+			     synopsis = 'Creates TetraChain model')
+	    register('tetraChain', t_desc, TetraScape.tetrahedral_model, logger=logger)
 
-	    m_desc = CmdDesc(required = [], 
-	    				optional = [("residues", ResiduesArg)], 
-	    				keyword = [("unit", FloatArg), ("alpha", FloatArg), ("tetra", BoolArg)], 
-	    				synopsis = 'create tetrahedral massing model')
-	    register('massing', m_desc, cmd.massing_model, logger=logger)
+	    m_desc = CmdDesc(required = [],
+			     optional = [("residues", ResiduesArg)],
+			     keyword = [("unit", FloatArg), ("alpha", FloatArg), ("checkOverlap", BoolArg), ("tetraChain", BoolArg), ("tetraChainResidues", ResiduesArg)],
+			     synopsis = 'Creates TetraMass model')
+	    register('tetraMass', m_desc, TetraScape.massing_model, logger=logger)
 
 bundle_api = _TetraAPI()
