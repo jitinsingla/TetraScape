9770c8ed44258b37110831dd361de5617d7821f3
Jitin Singla <jitin2025@gmail.com>
2024-08-15 18:05:38 +0530

    Add files via upload

---
+++ src/__init__.py
@@ -0,0 +1,23 @@
+from . import TetraScape
+from chimerax.atomic import ResiduesArg
+from chimerax.core.toolshed import BundleAPI
+from chimerax.core.commands import BoolArg, IntArg, CmdDesc, register, FloatArg
+
+class _TetraAPI(BundleAPI):
+	api_version = 1
+
+	@staticmethod
+	def register_command(bi, ci, logger):
+	    t_desc = CmdDesc(required = [],
+			     optional = [("residues", ResiduesArg), ("revSeq", BoolArg)],
+			     keyword = [],
+			     synopsis = 'Creates TetraChain model')
+	    register('tetraChain', t_desc, TetraScape.tetrahedral_model, logger=logger)
+
+	    m_desc = CmdDesc(required = [],
+			     optional = [("residues", ResiduesArg)],
+			     keyword = [("unit", FloatArg), ("alpha", FloatArg), ("checkOverlap", BoolArg), ("tetraChain", BoolArg), ("tetraChainResidues", ResiduesArg)],
+			     synopsis = 'Creates TetraMass model')
+	    register('tetraMass', m_desc, TetraScape.massing_model, logger=logger)
+
+bundle_api = _TetraAPI()
