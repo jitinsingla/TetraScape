20b8e4ffb5534a5380d62506999b810615837a33
Jitin Singla <jitin2025@gmail.com>
2024-08-16 13:36:12 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from . import TetraScape
+from . import TetraScape as TS
 from chimerax.atomic import ResiduesArg
 from chimerax.core.toolshed import BundleAPI
 from chimerax.core.commands import BoolArg, IntArg, CmdDesc, register, FloatArg
@@ -12,12 +12,12 @@ class _TetraAPI(BundleAPI):
 			     optional = [("residues", ResiduesArg), ("revSeq", BoolArg)],
 			     keyword = [],
 			     synopsis = 'Creates TetraChain model')
-	    register('tetraChain', t_desc, TetraScape.tetrahedral_model, logger=logger)
+	    register('tetraChain', t_desc, TS.tetrahedral_model, logger=logger)
 
 	    m_desc = CmdDesc(required = [],
 			     optional = [("residues", ResiduesArg)],
 			     keyword = [("unit", FloatArg), ("alpha", FloatArg), ("checkOverlap", BoolArg), ("tetraChain", BoolArg), ("tetraChainResidues", ResiduesArg)],
 			     synopsis = 'Creates TetraMass model')
-	    register('tetraMass', m_desc, TetraScape.massing_model, logger=logger)
+	    register('tetraMass', m_desc, TS.massing_model, logger=logger)
 
 bundle_api = _TetraAPI()
